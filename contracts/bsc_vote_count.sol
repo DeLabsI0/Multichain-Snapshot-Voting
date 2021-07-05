ba4d8132522a1203c06522aee0e2383b8390e323
timur-samarkand <86270019+timur-samarkand@users.noreply.github.com>
2021-07-05 11:34:19 -0700

    Finishes pancake swap pair

--- contracts/bsc_vote_count.sol
+++ contracts/bsc_vote_count.sol
@@ -25,6 +25,7 @@ contract BadgerVotingShareTest1 {
     IERC20 constant badger = IERC20(0x753fbc5800a8C8e3Fb6DC6415810d627A387Dfc9);
     ISett constant sett_badger = ISett(0x1F7216fdB338247512Ec99715587bb97BBf96eae);
     IPancakePair constant badger_BTC_Pancake = IPancakePair(0x5A58609dA96469E9dEf3fE344bC39B00d18eb9A5);
+    IPancakePair constant badger_wBNB_Pancake = IPancakePair(0xFd6ba353459153ab5193AAdF794EC0b2Fc99a850);
 
     function decimals() external pure returns (uint8) {
         return uint8(18);
@@ -43,22 +44,18 @@ contract BadgerVotingShareTest1 {
     }
 
     function _pancakeSwapBalanceOf(address _voter) internal view returns (uint) {
-        (, uint112 reserve1, ) = badger_BTC_Pancake.getReserves();
-        uint totalPancakeBalance = badger_BTC_Pancake.balanceOf(_voter);
-        return totalPancakeBalance * reserve1 / badger_BTC_Pancake.totalSupply();
-    }
-
-    /*
-        TODO: Count the votes in all places where badger can be held.
-        This includes staked supply and SETT deposited supply.
-    */
-    function _badgerBalanceOf(address _voter) internal view returns(uint) {
-        return badger.balanceOf(_voter);
+        (uint112 BTC_reserve, , ) = badger_BTC_Pancake.getReserves();
+        (uint112 wBNB_reserve, , ) = badger_wBNB_Pancake.getReserves();
+        uint badgerInBTCPancakePool = badger_BTC_Pancake.balanceOf(_voter)
+            * BTC_reserve / badger_BTC_Pancake.totalSupply();
+        uint badgerInwBNBPancakePool = badger_wBNB_Pancake.balanceOf(_voter)
+            * wBNB_reserve / badger_wBNB_Pancake.totalSupply();            
+        return badgerInBTCPancakePool + badgerInwBNBPancakePool;
     }
 
     function balanceOf(address _voter) external view returns (uint) {
         uint bBadgerPricePerShare = sett_badger.getPricePerFullShare();
-        return _badgerBalanceOf(_voter)
+        return badger.balanceOf(_voter)
             + sett_badger.balanceOf(_voter) * bBadgerPricePerShare / 1e18
             + _pancakeSwapBalanceOf(_voter);
     }
