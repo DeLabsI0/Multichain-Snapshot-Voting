bf623d3fd1e0942e25f208df9946e5a0fd501158
Tariq Patanam <t.ahmed.p@gmail.com>
2021-07-05 19:00:22 +0200

    Add PancakePair balance counting.

--- contracts/bsc_vote_count.sol
+++ contracts/bsc_vote_count.sol
@@ -7,11 +7,24 @@ interface IERC20 {
     function balanceOf(address account) external view returns (uint);
 }
 
+interface ISett {
+    function totalSupply() external view returns (uint);
+    function balanceOf(address owner) external view returns (uint);
+    function getPricePerFullShare() external view returns(uint);
+}
+
+interface IPancakePair {
+    function decimals() external pure returns (uint8);
+    function totalSupply() external view returns (uint);
+    function balanceOf(address owner) external view returns (uint);
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+}
+
 contract BadgerVotingShareTest1 {
 
-    // bbadger is the BADGER token on BSC.
-    // TODO: Right now we're using the address of USDC on BSC testnet to test our contract.
-    IERC20 constant bbadger = IERC20(0x9780881Bf45B83Ee028c4c1De7e0C168dF8e9eEF);
+    IERC20 constant badger = IERC20(0x753fbc5800a8C8e3Fb6DC6415810d627A387Dfc9);
+    ISett constant sett_badger = ISett(0x1F7216fdB338247512Ec99715587bb97BBf96eae);
+    IPancakePair constant badger_BTC_Pancake = IPancakePair(0x5A58609dA96469E9dEf3fE344bC39B00d18eb9A5);
 
     function decimals() external pure returns (uint8) {
         return uint8(18);
@@ -26,19 +39,28 @@ contract BadgerVotingShareTest1 {
     }
 
     function totalSupply() external view returns (uint) {
-        return bbadger.totalSupply();
+        return badger.totalSupply();
+    }
+
+    function _pancakeSwapBalanceOf(address _voter) internal view returns (uint) {
+        (, uint112 reserve1, ) = badger_BTC_Pancake.getReserves();
+        uint totalPancakeBalance = badger_BTC_Pancake.balanceOf(_voter);
+        return totalPancakeBalance * reserve1 / badger_BTC_Pancake.totalSupply();
     }
 
     /*
-        TODO: Count the votes in all places where bbadger can be held.
+        TODO: Count the votes in all places where badger can be held.
         This includes staked supply and SETT deposited supply.
     */
     function _badgerBalanceOf(address _voter) internal view returns(uint) {
-        return bbadger.balanceOf(_voter);
+        return badger.balanceOf(_voter);
     }
 
     function balanceOf(address _voter) external view returns (uint) {
-        return _badgerBalanceOf(_voter);
+        uint bBadgerPricePerShare = sett_badger.getPricePerFullShare();
+        return _badgerBalanceOf(_voter)
+            + sett_badger.balanceOf(_voter) * bBadgerPricePerShare / 1e18
+            + _pancakeSwapBalanceOf(_voter);
     }
 
     constructor() {}
